a5b9e147ff451ecbd820ec90982dc21f616d7dc3
EnriqueOrtiz27 <enriqueortizcasillas@gmail.com>
2019-10-28 16:27:26 -0600

    Seeding corregido

--- module-1/lab-mysql/your-code/seeding.sql
+++ module-1/lab-mysql/your-code/seeding.sql
@@ -1,9 +1,16 @@
 use lab_mysql;
 
-INSERT INTO cars() values(001, 020, 'Volvo', 2019, 'Gray', 200000, 002, 034, 'BMW', 2015, 'Black', 400000, 003, 027, 'Ferrari', 2012, 'Red', 1000000)
+INSERT INTO cars() values(001, 020, 'Volvo', 2019, 'Gray', 200000);
+INSERT INTO cars() values(002, 034, 'BMW', 2015, 'Black', 400000);
+INSERT INTO cars() values(003, 027, 'Ferrari', 2012, 'Red', 1000000);
 
-INSERT INTO customers() values(001, 222, 'Carlos D.', 5540402020, 'carlos@carlos.com', 'Casa de Carlos', 'Ciudad Carlos', 'País de Carlos', 'Volvo', 020, 002, 428, 'Jonathan S.', 22244356, 'jonathan@jonathan.com', 'Casa de Jonathan', 'Atizapán', 'Mexico', 'Ferrari', 003)
+INSERT INTO customers() values(001, 222, 'Carlos D.', 5540402020, 'carlos@carlos.com', 'Casa de Carlos', 'Ciudad Carlos', 'País de Carlos', 'Volvo', 020);
 
-INSERT INTO invoices() values(2019-01-15, 020, 'Volvo', 222, 3457,  2012-05-22, 027, 'Ferrari', 428, 2459)
+INSERT INTO customers() values(002, 428, 'Jonathan S.', 22244356, 'jonathan@jonathan.com', 'Casa de Jonathan', 'Atizapán', 'Mexico', 'Ferrari', 003);
 
-INSERT INTO salespersons() values(3457, 'Jennifer', 'Madrid', 2459, 'Michael', 'Amsterdam)
+INSERT INTO salespersons() values(3457, 'Jennifer', 'Madrid');
+INSERT INTO salespersons() values(2459, 'Michael', 'Amsterdam');
+
+
+INSERT INTO invoices() values('2010-01-15', 020, 'Volvo', 222, 3457);
+INSERT INTO invoices() values('2012-05-22', 027, 'Ferrari', 428, 2459);
